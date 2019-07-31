c674bf359477361f6cd87151df97ab8eb1843bb6
whilson03 <whilson03@gmail.com>
2019-08-01 00:41:03 +0100

    navigation drawer badges done

--- app/src/main/java/com/olabode/wilson/notekeep/MainActivity.java
+++ app/src/main/java/com/olabode/wilson/notekeep/MainActivity.java
@@ -5,18 +5,25 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.StrictMode;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
+import androidx.core.view.MenuItemCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 
 import com.google.android.material.navigation.NavigationView;
+import com.olabode.wilson.notekeep.viewmodels.NoteViewModel;
 import com.olabode.wilson.notekeep.views.FavouritesFragment;
 import com.olabode.wilson.notekeep.views.NotesFragment;
 import com.olabode.wilson.notekeep.views.SettingsFragment;
@@ -30,6 +37,10 @@ public class MainActivity extends AppCompatActivity
     private NavigationView navigationView;
     private FragmentTransaction ft;
 
+    private NoteViewModel noteViewModel;
+
+    private TextView note, favourite, trash;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +50,7 @@ public class MainActivity extends AppCompatActivity
         setSupportActionBar(toolbar);
 
         mHandler = new Handler();
+        noteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
 
 
         mDrawerLayout = findViewById(R.id.drawer_layout);
@@ -48,6 +60,14 @@ public class MainActivity extends AppCompatActivity
         mDrawerLayout.addDrawerListener(toggle);
         toggle.syncState();
         navigationView.setNavigationItemSelectedListener(this);
+
+        note = (TextView) MenuItemCompat.getActionView(navigationView.getMenu().findItem(R.id.nav_notes));
+        favourite = (TextView) MenuItemCompat.getActionView(navigationView.getMenu().findItem(R.id.nav_favourites));
+        trash = (TextView) MenuItemCompat.getActionView(navigationView.getMenu().findItem(R.id.nav_trash));
+
+        initializeCountDrawer();
+
+
 
 
         if (savedInstanceState == null) {
@@ -75,7 +95,7 @@ public class MainActivity extends AppCompatActivity
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
@@ -92,13 +112,12 @@ public class MainActivity extends AppCompatActivity
 
     /**
      * update the menu_favourite_frag screen as new fragment.
-     *
      * @param item
      * @return
      */
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         // Handle navigation view item clicks here.
         final int id = item.getItemId();
         // using handler to make drawer closing transition smoother.
@@ -187,4 +206,51 @@ public class MainActivity extends AppCompatActivity
         }
 
     }
+
+
+    private void initializeCountDrawer() {
+        noteViewModel.getNoteCount().observe(this, new Observer<Integer>() {
+            @Override
+            public void onChanged(Integer integer) {
+                note.setGravity(Gravity.CENTER_VERTICAL);
+                //note.setTypeface(null, Typeface.BOLD);
+                note.setTextColor(getResources().getColor(R.color.colorAccent));
+                if (integer > 99) {
+                    note.setText("99+");
+                } else {
+                    note.setText(String.valueOf(integer));
+                }
+            }
+        });
+        noteViewModel.getFavouriteCount().observe(this, new Observer<Integer>() {
+            @Override
+            public void onChanged(Integer integer) {
+                favourite.setGravity(Gravity.CENTER_VERTICAL);
+                //note.setTypeface(null, Typeface.BOLD);
+                favourite.setTextColor(getResources().getColor(R.color.colorAccent));
+                if (integer > 99) {
+                    favourite.setText("99+");
+                } else {
+                    favourite.setText(String.valueOf(integer));
+                }
+            }
+        });
+
+
+        noteViewModel.getTrashCount().observe(this, new Observer<Integer>() {
+            @Override
+            public void onChanged(Integer integer) {
+                trash.setGravity(Gravity.CENTER_VERTICAL);
+                //note.setTypeface(null, Typeface.BOLD);
+                trash.setTextColor(getResources().getColor(R.color.colorAccent));
+                if (integer > 99) {
+                    trash.setText("99+");
+                } else {
+                    trash.setText(String.valueOf(integer));
+                }
+            }
+        });
+
+    }
+
 }
